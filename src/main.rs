23f87a7a3a9132fa73c3bdbcaa599c32d46d4de3
Richard Braakman <shuttlegit@gmail.com>
2022-07-17 20:02:12 +0300

    window title

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,29 @@
-use druid::widget::{Button, Flex, Label};
-use druid::{AppLauncher, LocalizedString, PlatformError, Widget, WidgetExt, WindowDesc};
+use druid::widget::{ Button, Flex, Label };
+use druid::{
+    AppLauncher, Data, LocalizedString, PlatformError,
+    Widget, WidgetExt, WindowDesc
+};
+
+const WINDOW_TITLE: LocalizedString<AppState>
+    = LocalizedString::new("CK3 spellcheck");
+
+#[derive(Clone, Data)]
+struct AppState {
+}
 
 fn main() -> Result<(), PlatformError> {
-    let main_window = WindowDesc::new(ui_builder);
-    let data = 1_u32;
+    let data = AppState { };
+    let main_window = WindowDesc::new(ui_builder)
+        .title(WINDOW_TITLE);
     AppLauncher::with_window(main_window)
         .use_simple_logger()
         .launch(data)
 }
 
-fn ui_builder() -> impl Widget<u32> {
-    let text = LocalizedString::new("hello-counter")
-        .with_arg("count", |data: &u32, _env| (*data).into());
+fn ui_builder() -> impl Widget<AppState> {
+    let text = LocalizedString::new("hello-counter");
     let label = Label::new(text).padding(5.0).center();
     let button = Button::new("increment")
-        .on_click(|_ctx, data, _env| *data += 1)
         .padding(5.0);
 
     Flex::column().with_child(label).with_child(button)
