a6cd0cf8d7caf766fe8f6f098e07403cd0a1ca14
Richard Braakman <shuttlegit@gmail.com>
2022-07-17 22:04:25 +0300

    Put filename in the window title

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use druid::{
     AppLauncher, Data, LocalizedString, PlatformError,
     Widget, WidgetExt, WindowDesc
 };
+use std::borrow::Cow;
 use std::path::PathBuf;
 use std::rc::Rc;
 
@@ -13,21 +14,26 @@ struct Cli {
     pathname: PathBuf,
 }
 
-const WINDOW_TITLE: LocalizedString<AppState>
-    = LocalizedString::new("CK3 spellcheck");
+const WINDOW_TITLE: &str = "CK3 spellcheck";
 
 #[derive(Clone, Data)]
 struct AppState {
     pathname: Rc<PathBuf>,
+    filename: Rc<String>,
 }
 
 fn main() -> Result<(), PlatformError> {
     let args = Cli::parse();
+    let filename = match args.pathname.file_name() {
+        Some(name) => { name.to_string_lossy() }
+        None => { Cow::from("") }
+    }.to_string();
     let data = AppState {
         pathname: Rc::new(args.pathname),
+        filename: Rc::new(filename),
     };
     let main_window = WindowDesc::new(ui_builder)
-        .title(WINDOW_TITLE);
+        .title(WINDOW_TITLE.to_owned() + " " + data.filename.as_ref());
     AppLauncher::with_window(main_window)
         .use_simple_logger()
         .launch(data)
