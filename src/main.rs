cfb055215e3672ae1cb03ba59f9e5c6c81558a95
Richard Braakman <shuttlegit@gmail.com>
2022-07-18 10:14:31 +0300

    Read the file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use anyhow::{ Context, Result };
 use clap::Parser;
+use druid::text::RichText;
 use druid::widget::{ Button, Flex, Label };
 use druid::{
     AppLauncher, Data, LocalizedString, PlatformError,
@@ -20,6 +22,7 @@ const WINDOW_TITLE: &str = "CK3 spellcheck";
 struct AppState {
     pathname: Rc<PathBuf>,
     filename: Rc<String>,
+    text: RichText,
 }
 
 fn main() -> Result<(), PlatformError> {
@@ -28,9 +31,12 @@ fn main() -> Result<(), PlatformError> {
         Some(name) => { name.to_string_lossy() }
         None => { Cow::from("") }
     }.to_string();
+    let contents = std::fs::read_to_string(&args.pathname)
+        .with_context(|| format!("could not read file {}", args.pathname.display()))?;
     let data = AppState {
         pathname: Rc::new(args.pathname),
         filename: Rc::new(filename),
+        text: RichText::new(contents.into()),
     };
     let main_window = WindowDesc::new(ui_builder)
         .title(WINDOW_TITLE.to_owned() + " " + data.filename.as_ref());
