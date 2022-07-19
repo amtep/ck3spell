82f68eae51e018088c73f2b4ee35d75b2bf1b004
Richard Braakman <shuttlegit@gmail.com>
2022-07-19 10:15:20 +0300

    Display the file as a series of lines
    Also remember the line endings for each line

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use anyhow::{Context, Result};
 use clap::Parser;
-use druid::widget::{Flex, Scroll, TextBox};
+use druid::widget::{Flex, Label, LineBreaking, List, Scroll};
 use druid::{AppLauncher, Data, Lens, Widget, WidgetExt, WindowDesc};
 use std::path::PathBuf;
 use std::rc::Rc;
@@ -14,12 +14,27 @@ struct Cli {
 
 const WINDOW_TITLE: &str = "CK3 spellcheck";
 
+#[derive(Clone, Data, PartialEq)]
+#[allow(clippy::upper_case_acronyms)]
+enum LineEnd {
+    NL,
+    CRLF,
+    Nothing,
+}
+
+#[derive(Clone, Data, Lens)]
+struct Line {
+    line: String,
+    line_end: LineEnd,
+}
+
 #[derive(Clone, Data, Lens)]
 struct AppState {
+    /// File to spell check.
     pathname: Rc<PathBuf>,
+    /// Name of file to spell check, for display.
     filename: Rc<String>,
-    text: Arc<String>,
-    lines_crlf: bool,
+    lines: Arc<Vec<Line>>,
 }
 
 fn main() -> Result<()> {
@@ -37,16 +52,34 @@ fn main() -> Result<()> {
     if contents.starts_with('\u{feff}') {
         contents.remove(0); // Remove BOM
     }
-    let crlf = contents.contains("\r\n");
-    if crlf {
-        contents = contents.replace("\r\n", "\n");
+
+    let mut lines: Vec<Line> = Vec::new();
+    let mut line_iter = contents.split('\n').peekable();
+    while let Some(line) = line_iter.next() {
+        if line_iter.peek().is_none() {
+            if !line.is_empty() {
+                lines.push(Line {
+                    line: line.to_string(),
+                    line_end: LineEnd::Nothing,
+                });
+            }
+        } else if line.ends_with('\r') {
+            lines.push(Line {
+                line: line.strip_suffix('\r').unwrap().to_string(),
+                line_end: LineEnd::CRLF,
+            });
+        } else {
+            lines.push(Line {
+                line: line.to_string(),
+                line_end: LineEnd::NL,
+            });
+        }
     }
 
     let data = AppState {
         pathname: Rc::new(args.pathname),
         filename: Rc::new(filename),
-        text: Arc::new(contents),
-        lines_crlf: crlf,
+        lines: Arc::new(lines),
     };
     let main_window = WindowDesc::new(ui_builder())
         .title(WINDOW_TITLE.to_owned() + " " + data.filename.as_ref());
@@ -56,8 +89,12 @@ fn main() -> Result<()> {
         .with_context(|| "could not launch application")
 }
 
-fn ui_builder() -> impl Widget<AppState> {
-    let textbox = TextBox::multiline().lens(AppState::text).expand_width();
+fn make_line_item() -> impl Widget<Line> {
+    Label::dynamic(|line: &Line, _| line.line.to_string())
+        .with_line_break_mode(LineBreaking::WordWrap)
+}
 
-    Scroll::new(Flex::column().with_child(textbox)).vertical()
+fn ui_builder() -> impl Widget<AppState> {
+    let lines = List::new(make_line_item).lens(AppState::lines);
+    Scroll::new(Flex::column().with_child(lines)).vertical()
 }
