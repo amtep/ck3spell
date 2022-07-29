1d5a2ae188c13e04499e02354bc7c433c4c5588f
Richard Braakman <shuttlegit@gmail.com>
2022-07-29 19:38:12 +0300

    Parse FORBIDDENWORD line

--- speller/src/hunspell/affixdata.rs
+++ speller/src/hunspell/affixdata.rs
@@ -1,3 +1,7 @@
+use anyhow::{bail, Result};
+use itertools::Itertools;
+use std::num::ParseIntError;
+
 /// Represents the format of the flags after words in the dictionary file.
 pub enum FlagMode {
     /// Single-character flags
@@ -10,14 +14,47 @@ pub enum FlagMode {
     Utf8Flags,
 }
 
+pub type AffixFlag = u32;
+
+const DEFAULT_FORBIDDEN: AffixFlag = 0x110000;
+
 pub struct AffixData {
     pub flag_mode: FlagMode,
+    /// forbidden is the flag for invalid words.
+    pub forbidden: AffixFlag,
 }
 
 impl AffixData {
     pub fn new() -> Self {
         AffixData {
             flag_mode: FlagMode::CharFlags,
+            forbidden: DEFAULT_FORBIDDEN,
+        }
+    }
+
+    pub fn parse_flags(&self, flags: &str) -> Result<Vec<AffixFlag>> {
+        match self.flag_mode {
+            FlagMode::CharFlags | FlagMode::Utf8Flags => {
+                Ok(flags.chars().map(|c| c as u32).collect())
+            }
+            FlagMode::DoubleCharFlags => flags
+                .chars()
+                .chunks(2)
+                .into_iter()
+                .map(|mut pair| {
+                    let c1 = pair.next().unwrap() as u32;
+                    let c2 = pair.next().unwrap() as u32;
+                    if c1 > 255 || c2 > 255 {
+                        bail!("Invalid characters in double flag");
+                    }
+                    Ok(c1 * 256 + c2)
+                })
+                .collect(),
+            FlagMode::NumericFlags => flags
+                .split(',')
+                .map(|d| u32::from_str_radix(d, 10))
+                .collect::<Result<Vec<AffixFlag>, ParseIntError>>()
+                .map_err(anyhow::Error::from),
         }
     }
 }
--- speller/src/hunspell/parse_aff.rs
+++ speller/src/hunspell/parse_aff.rs
@@ -1,12 +1,14 @@
 /// Parser for hunspell-format .aff files
-use anyhow::{anyhow, Result};
+use anyhow::{anyhow, Error, Result};
 use nom::branch::alt;
-use nom::bytes::complete::tag;
-use nom::character::complete::{char, line_ending, space1};
-use nom::combinator::{eof, map, opt};
-use nom::error::ParseError;
+use nom::bytes::complete::{tag, take_till1};
+use nom::character::complete::{
+    char, line_ending, not_line_ending, space0, space1,
+};
+use nom::combinator::{eof, map, opt, success, value};
+use nom::error::{Error as NomError, ErrorKind, ParseError};
 use nom::multi::many0;
-use nom::sequence::{delimited, terminated};
+use nom::sequence::{delimited, preceded, terminated};
 use nom::{Compare, Err, Finish, IResult, InputLength, Parser};
 
 use crate::hunspell::AffixData;
@@ -15,8 +17,52 @@ type Input<'a> = &'a str;
 
 const BYTE_ORDER_MARK: char = '\u{FEFF}';
 
+struct AffError {
+    message: String,
+}
+
+impl AffError {
+    fn new(message: &str) -> Self {
+        AffError {
+            message: message.to_string(),
+        }
+    }
+
+    fn wrapped(message: &str) -> Err<Self> {
+        Err::Error(Self::new(message))
+    }
+
+    fn from_nom(e: Err<NomError<Input>>) -> Err<Self> {
+        Err::Error(Self::new(&e.to_string()))
+    }
+}
+
+impl<'a> ParseError<Input<'a>> for AffError {
+    fn from_error_kind(input: Input, kind: ErrorKind) -> Self {
+        let message = format!("{:?}:\t{}\n", kind, input);
+        AffError { message }
+    }
+
+    fn append(input: Input, kind: ErrorKind, other: Self) -> Self {
+        other
+    }
+}
+
+impl ToString for AffError {
+    fn to_string(&self) -> String {
+        self.message.to_string()
+    }
+}
+
+fn from_anyhow(e: Error) -> Err<AffError> {
+    AffError::wrapped(&e.to_string())
+}
+
+#[derive(Clone)]
 enum AffixLine<'a> {
+    Empty,
     SetEncoding(&'a str),
+    SetForbidden(&'a str),
 }
 
 /// Parse a line starting with a keyword and then a value.
@@ -43,22 +89,65 @@ where
     }
 }
 
-fn set_encoding_line(s: &str) -> IResult<&str, AffixLine> {
-    map(keyword("SET", tag("UTF-8")), AffixLine::SetEncoding)(s)
+fn comment(s: &str) -> IResult<&str, ()> {
+    value((), preceded(char('#'), not_line_ending))(s)
 }
 
-fn line(s: &str) -> IResult<&str, AffixLine> {
-    alt((set_encoding_line,))(s)
+fn ending(s: &str) -> IResult<&str, (), AffError> {
+    value((), delimited(space0, opt(comment), line_ending))(s)
+        .map_err(AffError::from_nom)
 }
 
-fn affix_file(s: &str) -> IResult<&str, AffixData> {
+fn value_string(s: &str) -> IResult<&str, &str, AffError> {
+    take_till1(|c: char| c.is_whitespace())(s)
+}
+
+fn set_encoding_line(s: &str) -> IResult<&str, AffixLine, AffError> {
+    map(keyword("SET", value_string), AffixLine::SetEncoding)(s)
+}
+
+fn set_forbidden_line(s: &str) -> IResult<&str, AffixLine, AffError> {
+    map(
+        keyword("FORBIDDENWORD", value_string),
+        AffixLine::SetForbidden,
+    )(s)
+}
+
+fn line(s: &str) -> IResult<&str, AffixLine, AffError> {
+    alt((
+        set_encoding_line,
+        set_forbidden_line,
+        success(AffixLine::Empty),
+    ))(s)
+}
+
+fn affix_file(s: &str) -> IResult<&str, AffixData, AffError> {
     let (s, _) = opt(char(BYTE_ORDER_MARK)).parse(s)?; // discard BOM
 
     let mut d = AffixData::new();
-    let (s, lines) = many0(terminated(line, line_ending))(s)?;
+    let mut forbidden_seen = false;
+    let (s, lines) = many0(terminated(line, ending))(s)?;
     for l in lines.iter() {
         match l {
-            AffixLine::SetEncoding(_enc) => (), // Only UTF-8 is accepted anyway
+            AffixLine::Empty => (),
+            AffixLine::SetEncoding(enc) => {
+                if enc != &"UTF-8" {
+                    return Err(AffError::wrapped(&format!(
+                        "Unsupported encoding {}",
+                        enc
+                    )));
+                }
+            }
+            AffixLine::SetForbidden(f) => {
+                let fflag = d.parse_flags(f).map_err(from_anyhow)?;
+                if fflag.len() != 1 || forbidden_seen {
+                    return Err(AffError::wrapped(
+                        "Found multiple flags for FORBIDDENWORD",
+                    ));
+                }
+                d.forbidden = fflag[0];
+                forbidden_seen = true;
+            }
         };
     }
     let (s, _) = eof(s)?;
